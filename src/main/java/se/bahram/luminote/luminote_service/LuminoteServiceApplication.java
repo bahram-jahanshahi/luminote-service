6d7761476ec74f82aa38e919ed505f5016c19278
Bahram Jahanshahi <bahram.jahanshahi@gmail.com>
2025-08-06 22:00:48 +0200

    Init

---
+++ src/main/java/se/bahram/luminote/luminote_service/LuminoteServiceApplication.java
@@ -0,0 +1,13 @@
+package se.bahram.luminote.luminote_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuminoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuminoteServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/bahram/luminote/luminote_service/LuminoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.bahram.luminote.luminote_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuminoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
